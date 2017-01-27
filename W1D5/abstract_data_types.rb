2760c13a2abf8d71125f1bc65efd063df0182bfa
aliciasavelly <alicia.savelly@gmail.com>
2017-01-26 21:43:01 -0800

    add Stack class

--- W1D5/abstract_data_types.rb
+++ W1D5/abstract_data_types.rb
@@ -1,18 +1,52 @@
 class Stack
   def initialize
-
+    @stack = []
   end
 
   def add(el)
-
+    @stack << el
   end
 
   def remove
+    @stack.pop
+  end
+
+  def show
+    @stack
+  end
+
+end
+
+# new_stack = Stack.new
+# new_stack.add(1)
+# new_stack.add(2)
+# new_stack.add(3)
+# new_stack.add(4)
+# p new_stack.show
+# new_stack.remove
+# new_stack.remove
+# p new_stack.show
+# new_stack.add(5)
+# new_stack.add(6)
+# p new_stack.show
+# new_stack.remove
+# p new_stack.show
+
+class Queue
+  def initialize
+    @queue = []
+  end
+
+  def enqueue(el)
+    @queue
+  end
+
+  def dequeue
 
   end
 
   def show
 
   end
-  
+
 end
