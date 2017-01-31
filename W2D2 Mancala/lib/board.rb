59bee1753084ce45e2ce25319d369a77f9e9dfd4
aliciasavelly <alicia.savelly@gmail.com>
2017-01-30 23:18:56 -0800

    pass each spec

--- W2D2 Mancala/lib/board.rb
+++ W2D2 Mancala/lib/board.rb
@@ -40,11 +40,25 @@ class Board
     #
     #   @cups[i + count] << :stone
     # end
+    stones = @cups[start_pos]
+    @cups[start_pos] = []
 
-    
+    cup_idx = start_pos
+    until stones.empty?
+      cup_idx += 1
+      cup_idx = 0 if cup_idx > 13
+      if cup_idx == 6
+        @cups[6] << stones.pop if current_player_name == @name1
+      elsif cup_idx == 13
+        @cups[13] << stones.pop if current_player_name == @name2
+      else
+        @cups[cup_idx] << stones.pop
+      end
+    end
+
 
     render
-    next_turn(end_pos)
+    next_turn(cup_idx)
   end
 
   def next_turn(ending_cup_idx)
