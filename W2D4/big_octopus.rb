0c221c805bea0312376d899597ebf87e1178c22b
aliciasavelly <alicia.savelly@gmail.com>
2017-02-01 20:13:25 -0800

    add slow dance fast dance

--- W2D4/big_octopus.rb
+++ W2D4/big_octopus.rb
@@ -71,9 +71,26 @@ tiles_array = ["up", "right-up", "right", "right-down",
               "down", "left-down", "left",  "left-up" ]
 
 def slow_dance(tile_direction, tiles_array)
-  
+  tiles_array.each_with_index do |tile, idx|
+    return idx if tile == tile_direction
+  end
 end
 
-def constant_dance
+p slow_dance("up", tiles_array) == 0
+p slow_dance("right-down", tiles_array) == 3
 
+tiles_hash = {"up" => 0,
+              "right-up" => 1,
+              "right" => 2,
+              "right-down" => 3,
+              "down" => 4,
+              "left-down" => 5,
+              "left" => 6,
+              "left-up" => 7}
+
+def fast_dance(tile_direction, tiles_hash)
+  tiles_hash[tile_direction]
 end
+
+p fast_dance("up", tiles_hash) == 0
+p fast_dance("right-down", tiles_hash) == 3
