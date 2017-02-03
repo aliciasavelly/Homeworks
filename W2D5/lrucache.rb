b2dd4ff9f091be4ebbacb1077673e892cac34ccf
aliciasavelly <alicia.savelly@gmail.com>
2017-02-03 09:11:40 -0800

    implement working LRU cache

--- W2D5/lrucache.rb
+++ W2D5/lrucache.rb
@@ -1,20 +1,50 @@
 class LRUCache
-    def initialize
+    def initialize(size)
+      @size = size
+      @cache = []
     end
 
     def count
       # returns number of elements currently in cache
+      @cache.length
     end
 
     def add(el)
       # adds element to cache according to LRU principle
+      if @cache.count == @initial_count
+        if @cache.include?(el)
+          @cache.delete(el)
+        else
+          @cache.shift
+        end
+      end
+      @cache << el
     end
 
     def show
       # shows the items in the cache, with the LRU item first
+      p @cache
+      nil
     end
 
-    private
-    # helper methods go here!
-
   end
+
+johnny_cache = LRUCache.new(4)
+
+johnny_cache.add("I walk the line")
+johnny_cache.add(5)
+
+p johnny_cache.count == 2 # => returns 2
+
+johnny_cache.add([1,2,3])
+johnny_cache.add(5)
+johnny_cache.add(-5)
+johnny_cache.add({a: 1, b: 2, c: 3})
+johnny_cache.add([1,2,3,4])
+johnny_cache.add("I walk the line")
+johnny_cache.add(:ring_of_fire)
+johnny_cache.add("I walk the line")
+johnny_cache.add({a: 1, b: 2, c: 3})
+
+
+johnny_cache.show # => prints [[1, 2, 3, 4], :ring_of_fire, "I walk the line", {:a=>1, :b=>2, :c=>3}]
