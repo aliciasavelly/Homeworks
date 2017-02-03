db5a39ca18651b2a71ebd929b145a1580e0d4647
aliciasavelly <alicia.savelly@gmail.com>
2017-02-03 09:16:09 -0800

    clean up LRU cache code

--- W2D5/lrucache.rb
+++ W2D5/lrucache.rb
@@ -1,40 +1,38 @@
 class LRUCache
-    def initialize(size)
-      @size = size
-      @cache = []
-    end
-
-    def count
-      # returns number of elements currently in cache
-      @cache.length
-    end
-
-    def add(el)
-      # adds element to cache according to LRU principle
-      if @cache.count == @initial_count
-        if @cache.include?(el)
-          @cache.delete(el)
-        else
-          @cache.shift
-        end
-      end
-      @cache << el
-    end
-
-    def show
-      # shows the items in the cache, with the LRU item first
-      p @cache
-      nil
-    end
-
+  def initialize(size)
+    @size = size
+    @cache = []
   end
+
+  def count
+    @cache.length
+  end
+
+  def add(el)
+    if @cache.count == @size
+      if @cache.include?(el)
+        @cache.delete(el)
+      else
+        @cache.shift
+      end
+    end
+
+    @cache << el
+  end
+
+  def show
+    p @cache
+    nil
+  end
+
+end
 
 johnny_cache = LRUCache.new(4)
 
 johnny_cache.add("I walk the line")
 johnny_cache.add(5)
 
-p johnny_cache.count == 2 # => returns 2
+johnny_cache.count == 2 # => returns 2
 
 johnny_cache.add([1,2,3])
 johnny_cache.add(5)
@@ -45,6 +43,5 @@ johnny_cache.add("I walk the line")
 johnny_cache.add(:ring_of_fire)
 johnny_cache.add("I walk the line")
 johnny_cache.add({a: 1, b: 2, c: 3})
-
 
 johnny_cache.show # => prints [[1, 2, 3, 4], :ring_of_fire, "I walk the line", {:a=>1, :b=>2, :c=>3}]
