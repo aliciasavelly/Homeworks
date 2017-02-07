811f9d0445341da70a037474f5a97e7a10d7c82a
aliciasavelly <alicia.savelly@gmail.com>
2017-02-06 23:16:14 -0800

    add self.all and initialize to playwright class

--- W3D2/plays_demo/plays.rb
+++ W3D2/plays_demo/plays.rb
@@ -61,7 +61,14 @@ end
 class Playwright
 
   def self.all
+    data = PlayDBConnection.instance.execute("SELECT * FROM playwrights")
+    data.map { |datum| Playwright.new(datum) }
+  end
 
+  def initialize(options)
+    @id = options['id']
+    @name = options['name']
+    @birth_year = options['birth_year']
   end
 
   def self.find_by_name(name)
