c885c269fe31c960cf3343a5029ff5ebd07e2e1c
aliciasavelly <alicia.savelly@gmail.com>
2017-02-07 23:28:17 -0800

    create seeds

--- W3D3/DemoProject/db/seeds.rb
+++ W3D3/DemoProject/db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+ranch_house = House.create(address: "Ranch House")
+beach_house = House.create(address: "Beach House")
+
+alicia = Person.create(name: "Alicia", house_id: beach_house)
+anna = Person.create(name: "Anna", house_id: beach_house)
+stephanie = Person.create(name: "Stephanie", house_id: beach_house)
+jesse = Person.create(name: "Jesse", house_id: ranch_house)
+zach = Person.create(name: "Zach", house_id: ranch_house)
+sresht = Person.create(name: "Sresht", house_id: ranch_house)
