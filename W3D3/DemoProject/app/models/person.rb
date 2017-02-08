feaff5eaa7fae489407a4f08001a4f904fa3aad8
aliciasavelly <alicia.savelly@gmail.com>
2017-02-07 23:46:52 -0800

    add more to person and house models. not working

--- W3D3/DemoProject/app/models/person.rb
+++ W3D3/DemoProject/app/models/person.rb
@@ -1,4 +1,6 @@
 class Person < ActiveRecord::Base
+  validates :name, presence: true, uniqueness: true
+  validates :house_id, presence: true
   belongs_to :house, {
     primary_key: :id,
     foreign_key: :house_id,
