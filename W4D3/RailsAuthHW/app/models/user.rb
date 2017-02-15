6f59bbd7e0995651f6221e518fd82f51be93050a
aliciasavelly <alicia.savelly@gmail.com>
2017-02-14 21:03:05 -0800

    migrate user migration and add validations to user class and write self.find_by_creds

--- W4D3/RailsAuthHW/app/models/user.rb
+++ W4D3/RailsAuthHW/app/models/user.rb
@@ -1,3 +1,29 @@
 class User < ApplicationRecord
-  validates 
+  validates :username, presence: true, uniqueness: true
+  validates :session_token, presence: true, uniqueness: true
+  validates :password_digest, presence: {message: "Password can't be blank"}
+  validates :password, length: {minimum: 6, allow_nil: true}
+  before_validation :ensure_session_token
+
+  def self.find_by_credentials(username, password)
+    user = User.find_by(username: username)
+    return user if user && BCrypt::Password.new(user.password_digest).is_password?(password)
+    nil
+  end
+
+  def self.generate_session_token
+
+  end
+
+  def reset_session_token!
+
+  end
+
+  def ensure_session_token
+
+  end
+
+  def password=(password)
+
+  end
 end
--- W4D3/RailsAuthHW/db/migrate/20170215044134_create_users.rb
+++ W4D3/RailsAuthHW/db/migrate/20170215044134_create_users.rb
@@ -5,7 +5,10 @@ class CreateUsers < ActiveRecord::Migration[5.0]
       t.string :password_digest, null: false
       t.string :session_token, null: false
 
-      t.timestamps
+      t.timestamps null: false
     end
+
+    add_index :users, :username, unique: true
+    add_index :users, :session_token, unique: true
   end
 end
