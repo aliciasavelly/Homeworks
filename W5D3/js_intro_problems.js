e1ae9b1f1d8456cc0b7aa385ed239bee96027f25
aliciasavelly <alicia.savelly@gmail.com>
2017-02-23 00:49:35 -0800

    hw w5d4

--- W5D3/js_intro_problems.js
+++ W5D3/js_intro_problems.js
@@ -12,7 +12,7 @@ isSubstring("this is super cool", "super");//true
 
 function fizzBuzz(array) {
   result = []
-  for(i = 0; i < array.length; i++) {
+  for(let i = 0; i < array.length; i++) {
     if((array[i] % 5 === 0) ^ (array[i] % 3 === 0)) {
       result.push(array[i]);
     }
@@ -21,3 +21,37 @@ function fizzBuzz(array) {
 };
 
 fizzBuzz([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 15]); //[ 3, 5, 6, 9, 10 ]
+
+function isPrime(number) {
+  if (number < 2) { return false; }
+
+  for(let i = 2; i < number; i++) {
+    if (number % i === 0) {
+      return false;
+    }
+  }
+  return true;
+};
+
+isPrime(2);//true
+isPrime(10);//false
+isPrime(15485863);//true
+isPrime(3548563);//false
+
+function sumOfNPrimes(number) {
+  let sum = 0;
+  let count = 0;
+  let i = 2;
+
+  for(let i = 2; count < number; i++) {
+    if(isPrime(i)) {
+      sum += i;
+      count += 1;
+    }
+  }
+  return sum;
+};
+
+sumOfNPrimes(0);//0
+sumOfNPrimes(1);//2
+sumOfNPrimes(4);//17
