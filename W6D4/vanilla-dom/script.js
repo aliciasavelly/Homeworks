497fa476235b0e25281fca1fbaaf3aac26377d95
aliciasavelly <alicia.savelly@gmail.com>
2017-02-28 23:44:17 -0800

    finish vanilla dom manipulation with dog photos and favourite places in sf

--- W6D4/vanilla-dom/script.js
+++ W6D4/vanilla-dom/script.js
@@ -18,14 +18,57 @@ document.addEventListener("DOMContentLoaded", () => {
 
   // adding SF places as list items
 
-  // --- your code here!
+  const handleFavForm = (e) => {
+    e.preventDefault();
 
+    const favInput = document.querySelector(".favorite-input");
+    const fav = favInput.value;
+    favInput.value = "";
 
+    const li = document.createElement("li");
+    li.textContent = fav;
+    const ul = document.getElementById("sf-places");
+
+    ul.appendChild(li);
+  };
+
+  const listSubmit = document.querySelector(".favorite-submit");
+  listSubmit.addEventListener("click", handleFavForm);
 
   // adding new photos
 
   // --- your code here!
 
+  const toggleNewPhotoForm = (e) => {
 
+    const photoFormDiv = document.querySelector(".photo-form-container")
+    if (photoFormDiv.className === "photo-form-container") {
+      photoFormDiv.className = "photo-form-container hidden";
+    } else {
+      photoFormDiv.className = "photo-form-containe";
+    }
+  }
 
+  const photoShowButton = document.querySelector(".photo-show-button");
+  photoShowButton.addEventListener("click", toggleNewPhotoForm);
+
+  const handleNewPhotoForm = (e) => {
+    e.preventDefault();
+
+    const photoUrlInput = document.querySelector(".photo-url-input");
+    const photoUrl = photoUrlInput.value;
+    photoUrlInput.value = "";
+
+    const newImg = document.createElement("img");
+    newImg.src = photoUrl;
+
+    const li = document.createElement("li");
+    li.appendChild(newImg);
+
+    const ul = document.querySelector(".dog-photos");
+    ul.appendChild(li);
+  }
+
+  const photoSubmitButton = document.querySelector(".photo-url-submit");
+  photoSubmitButton.addEventListener("click", handleNewPhotoForm);
 });
