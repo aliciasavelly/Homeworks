09eb030c2f97baf32c8ad471543f5982db1ef6aa
aliciasavelly <alicia.savelly@gmail.com>
2017-03-05 23:01:20 -0800

    create working calculator

--- W6D5/calculator_react2/calculator.jsx
+++ W6D5/calculator_react2/calculator.jsx
@@ -4,16 +4,71 @@ class Calculator extends React.Component{
   constructor(props){
     super(props);
     this.state = {num1: "", num2: "", result: 0};
+
+    this.setNum1 = this.setNum1.bind(this);
+    this.setNum2 = this.setNum2.bind(this);
+    this.clear = this.clear.bind(this);
+    this.add = this.add.bind(this);
+    this.subtract = this.subtract.bind(this);
+    this.multiply = this.multiply.bind(this);
+    this.divide = this.divide.bind(this);
   }
 
   //your code here
 
+  setNum1(e) {
+    const num1 = e.target.value ? parseInt(e.target.value) : "";
+    this.setState({ num1 });
+  }
+
+  setNum2(e) {
+    const num2 = e.target.value ? parseInt(e.target.value) : "";
+    this.setState({ num2 });
+  }
+
+  clear(e) {
+    e.preventDefault();
+    this.setState({ num1: "", num2: "", result: 0});
+  }
+
+  add(e) {
+    e.preventDefault();
+    const result = this.state.num1 + this.state.num2;
+    this.setState({ result });
+  }
+
+  subtract(e) {
+    e.preventDefault();
+    const result = this.state.num1 - this.state.num2;
+    this.setState({ result });
+  }
+
+  multiply(e) {
+    e.preventDefault();
+    const result = this.state.num1 * this.state.num2;
+    this.setState({ result });
+  }
+
+  divide(e) {
+    e.preventDefault();
+    const result = this.state.num1 / this.state.num2;
+    this.setState({ result });
+  }
+
   render(){
+    const { num1, num2, result } = this.state;
     return (
       <div>
-        <div>
-          <h1>{this.state.result}</h1>
-        </div>
+        <h1>{result}</h1>
+
+        <input onChange={this.setNum1} value={num1} />
+        <input onChange={this.setNum2} value={num2} />
+        <button onClick={this.clear}>Clear</button>
+        <br />
+        <button onClick={this.add}>Add</button>
+        <button onClick={this.subtract}>Subtract</button>
+        <button onClick={this.multiply}>Multiply</button>
+        <button onClick={this.divide}>Divide</button>
       </div>
     );
   }
