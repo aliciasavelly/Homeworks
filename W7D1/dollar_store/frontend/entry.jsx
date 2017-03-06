607327fb27ba5b3b25cf5506442bcd853d94be13
aliciasavelly <alicia.savelly@gmail.com>
2017-03-05 18:10:09 -0800

    add select currency action

--- W7D1/dollar_store/frontend/entry.jsx
+++ W7D1/dollar_store/frontend/entry.jsx
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom';
 import Widget from './components/widget';
 import store from './store';
 
+window.selectCurrency = selectCurrency;
+
 document.addEventListener("DOMContentLoaded", function(){
   ReactDOM.render(<Widget store={store} />, document.getElementById('root'));
 });
