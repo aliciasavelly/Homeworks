b146702bb3ddd21f6ece54b71803b9b0c0b07098
aliciasavelly <alicia.savelly@gmail.com>
2017-03-06 22:33:13 -0800

    create successful middleware

--- W7D2/middleware/frontend/todo_redux.jsx
+++ W7D2/middleware/frontend/todo_redux.jsx
@@ -12,3 +12,31 @@ document.addEventListener('DOMContentLoaded', () => {
   const root = document.getElementById('content');
   ReactDOM.render(<Root store={store} />, root);
 });
+
+//
+// const addLoggingToDispatch = store => next => action => {
+//   console.log('state pre-dispatch: ', store.getState());
+//   console.log(action);
+//
+//   let result = next(action);
+//
+//   console.log('state post-dispatch: ', store.getState());
+//   return result;
+// };
+//
+// const applyMiddlewares = (store, ...middlewares) => {
+//   let dispatch = store.dispatch;
+//   middlewares.forEach( middleware => dispatch = middleware(store)(dispatch) );
+//
+//   return Object.assign({}, store, { dispatch });
+// };
+//
+// document.addEventListener('DOMContentLoaded', () => {
+//   const preloadedState = localStorage.state ?
+//     JSON.parse(localStorage.state) : {};
+//   let store = configureStore(preloadedState);
+//   store = applyMiddlewares(store, addLoggingToDispatch);
+//
+//   const root = document.getElementById('content');
+//   ReactDOM.render(<Root store={store} />, root);
+// });
