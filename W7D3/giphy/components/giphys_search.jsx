a7d599646af84ee1c5a8d5d2f721284728429596
aliciasavelly <alicia.savelly@gmail.com>
2017-03-08 07:52:03 -0800

    create giphys index items and giphys index

--- W7D3/giphy/components/giphys_search.jsx
+++ W7D3/giphy/components/giphys_search.jsx
@@ -1,3 +1,40 @@
 import React from 'react';
 
 import GiphysIndex from './giphys_index';
+
+class GiphysSearch extends React.Component {
+  constructor() {
+    super();
+    this.state = { searchTerm: 'pitbull' };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  componentDidMount() {
+    this.props.fetchSearchGiphys('pitbull');
+  }
+
+  handleChange(e) {
+
+  }
+
+  handleSubmit(e) {
+
+  }
+
+  render() {
+    let { giphys } = this.props;
+
+    return (
+      <div>
+        <form className="search-bar">
+          <input value={this.state.searchTerm} onChange={this.handleChange} />
+          <button type="submit" onClick={this.handleSubmit}>Search Giphy</button>
+        </form>
+        <GiphysIndex giphys={giphys} />
+      </div>
+    )
+  }
+}
+
+export default GiphysSearch;
