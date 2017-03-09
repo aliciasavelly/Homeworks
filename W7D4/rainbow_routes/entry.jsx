20c37f8c68cd4e18f5806e287079f7c87071a07b
aliciasavelly <alicia.savelly@gmail.com>
2017-03-08 19:04:48 -0800

    allow clicking to add in colors

--- W7D4/rainbow_routes/entry.jsx
+++ W7D4/rainbow_routes/entry.jsx
@@ -26,20 +26,19 @@ class Rainbow extends React.Component {
   }
 
   addRed() {
-    console.log(router);
-    // this.props.router.push('/red');
+    this.props.router.push('/red');
   }
 
   addGreen() {
-    // your code here
+    this.props.router.push('/green');
   }
 
   addBlue() {
-    // your code here
+    this.props.router.push('/blue');
   }
 
   addViolet() {
-    // your code here
+    this.props.router.push('/violet');
   }
 
   render() {
