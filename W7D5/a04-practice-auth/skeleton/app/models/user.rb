9b3d14ea94258e1908806001ba2e10be9d183757
aliciasavelly <alicia.savelly@gmail.com>
2017-03-09 22:19:23 -0800

    write user model

--- W7D5/a04-practice-auth/skeleton/app/models/user.rb
+++ W7D5/a04-practice-auth/skeleton/app/models/user.rb
@@ -1,2 +1,35 @@
 class User < ActiveRecord::Base
+  validates :username, :password_digest, :session_token, presence: true
+  validates :password, length: {minimum: 6, allow_nil: true}
+
+  attr_reader :password
+  has_many :links
+  has_many :comments
+
+  before_validation :ensure_session_token
+
+  def password=(password)
+    @password = password
+    self.password_digest = BCrypt::Password.create(password)
+  end
+
+  def is_password?(password)
+    BCrypt::Password.new(self.password_digest).is_password?(password)
+  end
+
+  def self.generate_session_token
+    SecureRandom.urlsafe_base64(16)
+  end
+
+  def ensure_session_token
+    self.session_token ||= User.generate_session_token
+  end
+
+  def reset_session_token!
+    self.session_token = User.generate_session_token
+    self.save!
+    self.session_token
+  end
+
+
 end
